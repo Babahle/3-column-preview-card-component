7335dba836a296c2956db09df46eea73ca4fb63a
Babahle <mokonebabzini@gmail.com>
2021-08-28 09:27:21 +0200

    Installed Tailwind and Finished the challenge

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(31, 77%, 52%)',
+        secondary: 'hsl(184, 100%, 22%)',
+        tertiary: 'hsl(179, 100%, 13%)',
+        transparentWhite: ' hsla(0, 0%, 100%, 0.75)',
+        lightGray: 'hsl(0, 0%, 95%)'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
